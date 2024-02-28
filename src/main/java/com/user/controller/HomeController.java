ea7c8680f39d58ed3de1a17b9afc3f57ebf7b65f
Akhilesh Yadav <akhyad54321@gmail.com>
2024-02-28 19:57:29 +0530

    .

--- src/main/java/com/user/controller/HomeController.java
+++ src/main/java/com/user/controller/HomeController.java
@@ -1,16 +1,30 @@
 package com.user.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/home")
 public class HomeController {
 
-    @GetMapping
-    public String home(){
-        return "This is Home Page";
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String user1(){
+        return "This is User Page";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin(){
+        return "This is Admin Page";
+    }
+
+    @PreAuthorize("hasRole('Management')")
+    @GetMapping("/management")
+    public String management(){
+        return "This is Management Page";
     }
 
 }
